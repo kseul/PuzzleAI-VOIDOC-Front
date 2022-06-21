8ae79992bfbb2330ed8cbacafd336cc1cf11b5d1
seul <kseul06@gmail.com>
2022-06-21 11:42:14 +0900

    Add : react-native 초기 세팅

---
+++ src/screens/DotList/DitList.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const DitList = () => {
+  return <div></div>;
+};
+
+export default DitList;
---
+++ src/screens/RsrvtCalendar/RsrvtCalendar.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const RsrvtCalendar = () => {
+  return <div></div>;
+};
+
+export default RsrvtCalendar;
