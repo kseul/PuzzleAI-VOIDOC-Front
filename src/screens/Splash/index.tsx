41a5ba5477c9f7b1e625a899955f4110a6dfaaf3
SEONA LEE <dahyelee@SEONA-LEE-MacBookPro.local>
2022-06-24 15:07:30 +0900

    Error Fixed : SafeAreaProvider 연결
    - Splash 컴포넌트에 <SafeAreaView> 연결

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
 
 const Splash = () => {
   return (
-    <View>
-      <Text>Splash</Text>
-    </View>
+    <SafeAreaView>
+      <View>
+        <Text>Splash</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
