b69bdd2f29d29bab06109bc6b15e1824bdc87503
seul <kseul06@gmail.com>
2022-06-28 23:54:39 +0900

    Implement SignUp Function
    - 회원가입 기능 구현
    - 인풋 유효성 검사

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,6 @@
-export const BASE_URL = '';
+export const BASE_URL = 'http://10.58.6.44:8000';
+const API = {
+  signUp: `${BASE_URL}/users/signup`,
+  emailCheck: `${BASE_URL}/users/check_duplicate`,
+};
+export default API;
