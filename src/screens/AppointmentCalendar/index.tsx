a37af547f92acece59e9e3d6f73de4d3f7e58bcd
parkjooyoung <mirkduddl@naver.com>
2022-07-01 15:52:12 +0900

    Add : AppointmentCalendar Header 영역 UI 완료

--- src/screens/AppointmentCalendar/index.tsx
+++ src/screens/AppointmentCalendar/index.tsx
@@ -1,19 +1,38 @@
 import React from 'react';
 import {StyleSheet, Text, View, Image} from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { commonStyle } from "styles/commonStyle";
+import { commonStyle } from 'styles/commonStyle';
+import { theme } from 'styles/theme'
 
 const AppointmentCalendar = () => {
   return (
-      <SafeAreaView edges={['bottom', 'left', 'right']} style={[commonStyle.fullscreen]} >
-        <View>
-          <Text style={styles.border}>AppointmentCalendar</Text>
-          <View>
-            <Text style={styles.border}>profile image</Text>
-            <View style={styles.border}>
-              <Text>홍정의 선생님</Text>
-              <Text>피부과 전문의</Text>
-              <Text>퍼즐AI병원</Text>
+      <SafeAreaView 
+        edges={['bottom', 'left', 'right']} 
+        style={[commonStyle.fullscreen]} >
+        <View style={[
+          styles.flexDirectionRow, 
+          styles.border, 
+          styles.marginTop]}>
+          {/* <Image source={} /> */}
+          <Text style={[
+            styles.image, styles.marginRight
+            ]}>profile image</Text>
+          <View style={styles.marginBottom}>
+            <Text style={[
+              styles.profileTitleFontSize, 
+              styles.profileDocColor,
+              styles.marginBottomSamll
+              ]}>홍정의 선생님</Text>
+            <View style={styles.flexDirectionRow}>
+              <Text style={[
+                styles.profileDescFontSize, 
+                styles.profileDocColor,
+                styles.marginRightSmall
+                ]}>피부과 전문의</Text>
+              <Text style={[
+                styles.profileDescFontSize,
+                styles.profileHospGrayColor
+                ]}>퍼즐AI병원</Text>
             </View>
           </View>
         </View>
@@ -22,16 +41,57 @@ const AppointmentCalendar = () => {
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    // height: '100%',
-    justifyContent: 'space-between',
+  flexDirectionRow: {
+    flexDirection: 'row',
   },
+  
   border: {
-    borderWidth: 1,
-    borderColor: 'red',
+    width: '100%',
+    borderBottomWidth: 1,
+    borderColor: '#EEEEEE',
+  },
+  
+  image: {
+    width: 50,
+    height: 50,
+    backgroundColor: 'yellow'
+  },
+
+  marginTop: {
+    marginTop: 32,
+  },
+  
+  marginRight: {
+    marginRight: 20,
+  },
+
+  marginRightSmall: {
+    marginRight: 8,
+  },
+
+  marginBottom: {
+    marginBottom: 26,
+  },
+
+  marginBottomSamll: {
+    marginBottom: 5,
+  },
+  
+  profileTitleFontSize: {
+    fontSize: theme.fontSizes.fontRegular,
+  },
+
+  profileDescFontSize: {
+    fontSize: 13,
+  },
+
+  profileDocColor: {
+    color: theme.colors.docGray
+  },
+
+  profileHospGrayColor: {
+    color: theme.colors.docHospGray
   }
 })
 
 export default AppointmentCalendar;
-
