2550323fb9985d0023900bba54a7a3e178c93757
seul <kseul06@gmail.com>
2022-07-04 18:17:26 +0900

    Modify config Key
    - config.ts파일 mainAppointments -> departmentList 키 변경

--- src/config.ts
+++ src/config.ts
@@ -3,6 +3,6 @@ const API = {
   signUp: `${BASE_URL}/users/signup`,
   signIn: `${BASE_URL}/users/login`,
   emailCheck: `${BASE_URL}/users/check_duplicate`,
-  mainAppointments: `${BASE_URL}/appointments/departments`,
+  departmentList: `${BASE_URL}/appointments/departments`,
 };
 export default API;
