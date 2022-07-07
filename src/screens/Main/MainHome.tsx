dfcd0d0c7cc89faf2ba53553edbd698dbf5d65cb
seul <kseul06@gmail.com>
2022-07-08 01:02:01 +0900

    Modify AuthContext
    - mainHome.tsx 에서 userName 가져오는 로직 수정
    - AuthContext.tsx 수정 -> payload로 유저이름 데이터 전달 & 상태저장

--- src/screens/Main/MainHome.tsx
+++ src/screens/Main/MainHome.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useContext} from 'react';
 import {Text, View, Image, Pressable, FlatList} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {StyleSheet} from 'react-native';
@@ -8,33 +8,23 @@ import {commonStyle} from 'styles/commonStyle';
 import {theme} from 'styles/theme';
 import API from 'config';
 import useFetch from 'components/useFetch';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import {AuthContext} from 'AuthContext';
 
 const MainHome = ({navigation}: MainHomeScreenProps) => {
   const {width} = useWindowDimensions();
-  const [name, setName] = useState();
+
+  const {userState} = useContext(AuthContext);
+  const userName = userState.userName;
 
   const departmentListUrl = `${API.departmentList}`;
   const departmentListData = useFetch(departmentListUrl).result;
-
-  useEffect(() => {
-    const getUserName = async () => {
-      try {
-        const userName = await AsyncStorage.getItem('user_name');
-        setName(userName);
-      } catch (error) {
-        throw new Error('userName 가져오기 실패');
-      }
-    };
-    getUserName();
-  }, []);
 
   return (
     <SafeAreaView style={[commonStyle.fullscreen, styles.safeArea]}>
       <View style={styles.headerContainer}>
         <View style={[styles.headerText, styles.flexStyle]}>
           <Text style={[styles.nameStyle, styles.textSpace, styles.textColr]}>
-            {name}
+            {userName}
           </Text>
           <Text style={styles.nameText}>님 반갑습니다.</Text>
         </View>
