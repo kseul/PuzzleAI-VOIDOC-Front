0c9e57d15c03e8ced34b1aa33ec1b6b5a0063f1c
seul <kseul06@gmail.com>
2022-07-08 21:12:24 +0900

    Add docList screen
    - DoctorDataCard 컴포넌트 활용 UI 작성
    - 선택된 과목의 현재 페이지 주소로 API통신

--- src/screens/DocList/index.tsx
+++ src/screens/DocList/index.tsx
@@ -1,19 +1,65 @@
-import React, {useEffect} from 'react';
-import {Text, View} from 'react-native';
-import {DocListScreenProps} from 'types/type';
+import React, {useEffect, useState} from 'react';
+import {View, SafeAreaView, StyleSheet, FlatList} from 'react-native';
+import {theme} from 'styles/theme';
+import {DocListScreenProps, DocListProp} from 'types/type';
+import DoctorDataCard from 'components/DoctorDataCard';
+import {getToken} from 'AuthContext';
+import API from 'config';
 
 const DocList = ({route, navigation}: DocListScreenProps) => {
-  const {id, name, thumbnails} = route.params;
+  const {id, name} = route.params;
+  const [doctorListData, setDoctorListData] = useState();
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
     navigation.setOptions({title: name});
   }, []);
 
+  useEffect(() => {
+    const dataListFetch = async () => {
+      const token = await getToken();
+      const mainData = await fetch(
+        `${API.departmentList}/${id}?page=${currentPage}`,
+        {
+          method: 'GET',
+          headers: {
+            Authorization: token,
+          },
+        },
+      );
+      const res = await mainData.json();
+      const data = res.result;
+      setDoctorListData(data);
+    };
+    dataListFetch();
+  }, []);
+
   return (
-    <View>
-      <Text>{name}</Text>
-    </View>
+    <SafeAreaView style={styles.mainListWrapper}>
+      <FlatList
+        data={doctorListData}
+        renderItem={({item}: {item: DocListProp}) => (
+          <View style={styles.listContainer}>
+            <DoctorDataCard item={item} />
+          </View>
+        )}
+        keyExtractor={item => item.id.toString()}
+      />
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  mainListWrapper: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+
+  listContainer: {
+    marginHorizontal: 18,
+    borderBottomWidth: 1,
+    borderBottomColor: theme.colors.mainBorderGray,
+  },
+});
 
 export default DocList;
