05c42864917aaede6f4679c9d64f4d0a69e9ef40
seul <kseul06@gmail.com>
2022-07-13 16:14:38 +0900

    Modify AppointmentSubmit
    - AppointmentSubmit styles 정리

--- src/screens/AppointmentSubmit/index.tsx
+++ src/screens/AppointmentSubmit/index.tsx
@@ -22,7 +22,7 @@ const AppointmentSubmit = ({navigation}: AppointmentSubmitScreenProps) => {
   const {doctorInfo} = useContext(DoctorInfoContext);
 
   const onSubmit = () => {
-    // 진료예약 확정 페이지 merge 후 수정
+    // ToDo : 진료예약 확정 페이지 merge 후 수정
     // navigation.navigate('진료예약 확정 페이지');
   };
 
@@ -50,71 +50,11 @@ const AppointmentSubmit = ({navigation}: AppointmentSubmitScreenProps) => {
 };
 
 const styles = StyleSheet.create({
-  imageContainer: {
-    flexDirection: 'row',
-  },
   safeArea: {flex: 1},
-
-  submitContainer: {
-    marginBottom: 24,
-  },
-
-  title: {
-    paddingBottom: 6,
-    color: theme.colors.AppointmentGreen,
-    fontWeight: '600',
-  },
-
-  input: {
-    height: 48,
-    padding: 15,
-    color: theme.colors.AppointmentTimeTextGray,
-    borderColor: theme.colors.userGray,
-    backgroundColor: theme.colors.AppointmentTimeBgGray,
-  },
-
-  textInputArea: {
-    height: 203,
-    paddingHorizontal: 10,
-    paddingTop: 13,
-    textAlignVertical: 'top',
-    color: theme.colors.AppointmentInputTextGray,
-  },
-
-  imageWrapper: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: 106,
-    height: 106,
-    marginRight: 6,
-  },
-
-  inputBackground: {
-    backgroundColor: theme.colors.AppointmentInputBgGray,
-  },
-
-  inputText: {
-    fontSize: theme.fontSizes.fontRegular,
-  },
-
-  cameraImage: {
-    height: 25,
-    width: 28,
-  },
 
   submitBtn: {
     borderRadius: 8,
     backgroundColor: theme.colors.RstvtInnerLightGray,
-  },
-
-  deleteBtnWrapper: {
-    zIndex: 1,
-  },
-
-  deleteBtn: {
-    position: 'absolute',
-    right: 8,
-    top: 2,
   },
 });
 
